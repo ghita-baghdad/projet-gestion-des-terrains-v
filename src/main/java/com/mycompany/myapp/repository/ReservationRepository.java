67ade5efaf94332c31c5460abc8ba2e9bf9d8b36
ghita-baghdad <147449053+ghita-baghdad@users.noreply.github.com>
2024-01-14 17:52:54 +0100

    Add files via upload

--- src/main/java/com/mycompany/myapp/repository/ReservationRepository.java
+++ src/main/java/com/mycompany/myapp/repository/ReservationRepository.java
@@ -2,6 +2,7 @@ package com.mycompany.myapp.repository;
 
 import com.mycompany.myapp.domain.Reservation;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -9,4 +10,8 @@ import org.springframework.stereotype.Repository;
  */
 @SuppressWarnings("unused")
 @Repository
-public interface ReservationRepository extends JpaRepository<Reservation, Long> {}
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    @Query("SELECT c.nomClient FROM Reservation r JOIN r.nomClient c WHERE r.id = :reservationId")
+    String findNomClientByReservationId(@Param("reservationId") Long reservationId);
+
+}
